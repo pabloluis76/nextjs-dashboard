41faa3deaec7986c1ff4a706ae38add55d1eae3d
Pablo Luis <pabloluis76@gmail.com>
2023-11-29 20:20:28 -0500

    srtart the repo

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+
+import './ui/global.css';
+import { montserrat } from './ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
